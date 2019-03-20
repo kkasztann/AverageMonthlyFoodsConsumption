4a1d9a84aef566e712bc62d658097b810c7c9604
Daniel Slowik <kkasztann@gmail.com>
2019-03-21 00:30:13 +0100

    Map as inline SVG

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
 console.log("Work mode ON");
+//  All areas are listed in the line below. You can use this list in your script.
+// {id:"PL-DS"},{id:"PL-KP"},{id:"PL-LD"},{id:"PL-LU"},{id:"PL-LB"},{id:"PL-MA"},{id:"PL-MZ"},{id:"PL-OP"},{id:"PL-PK"},{id:"PL-PD"},{id:"PL-PM"},{id:"PL-SL"},{id:"PL-SK"},{id:"PL-WN"},{id:"PL-WP"},{id:"PL-ZP"}
