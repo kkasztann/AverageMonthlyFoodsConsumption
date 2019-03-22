83a5d17d249b4701ae80589fb90cd17d08355372
Daniel Slowik <kkasztann@gmail.com>
2019-03-23 00:43:19 +0100

    Added choosing land

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 console.log("Work mode ON");
 //  All areas are listed in the line below. You can use this list in your script.
-// {id:"PL-DS"},{id:"PL-KP"},{id:"PL-LD"},{id:"PL-LU"},{id:"PL-LB"},{id:"PL-MA"},{id:"PL-MZ"},{id:"PL-OP"},{id:"PL-PK"},{id:"PL-PD"},{id:"PL-PM"},{id:"PL-SL"},{id:"PL-SK"},{id:"PL-WN"},{id:"PL-WP"},{id:"PL-ZP"}
+// ,{id:"PL-LU"},{id:"PL-LB"},
+
+const voivodeships = new Map([
+    ["PL-DS", "dolnośląskie"],
+    ["PL-KP", "kujawsko-pomorskie"],
+    ["PL-LD", "lubelskie"],
+    ["PL-LU", "lubuskie"],
+    ["PL-LB", "łódzkie"],
+    ["PL-MA", "małopolskie"],
+    ["PL-MZ", "mazowieckie"],
+    ["PL-OP", "opolskie"],
+    ["PL-PK", "podkarpackie"],
+    ["PL-PD", "podlaskie"],
+    ["PL-PM", "pomorskie"],
+    ["PL-SL", "śląskie"],
+    ["PL-SK", "świętokrzyskie"],
+    ["PL-WN", "warmińsko-mazurskie"],
+    ["PL-WP", "wielkopolskie"],
+    ["PL-ZP", "zachodniopomorskie"]
+]);
+
+const map = document.querySelectorAll("path");
+const voivodeship = document.querySelector(".voivodeship");
+map.forEach(land => {
+    land.addEventListener("click", (e) => {
+        map.forEach(land => {
+            land.classList.remove("landTagged");
+        });
+        e.target.classList.add("landTagged");
+        voivodeship.textContent = voivodeships.get(e.target.id);
+    })
+})
