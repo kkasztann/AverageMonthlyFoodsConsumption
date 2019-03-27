97a0f741c92c759e4672d1f8b48a30af6e61a3df
Daniel Slowik <kkasztann@gmail.com>
2019-03-28 00:15:55 +0100

    Collect voivodeships ID

--- js/main.js
+++ js/main.js
@@ -1,34 +1,57 @@
-console.log("Work mode ON");
-//  All areas are listed in the line below. You can use this list in your script.
-// ,{id:"PL-LU"},{id:"PL-LB"},
-
 const voivodeships = new Map([
-    ["PL-DS", "dolnośląskie"],
-    ["PL-KP", "kujawsko-pomorskie"],
-    ["PL-LD", "lubelskie"],
-    ["PL-LU", "lubuskie"],
-    ["PL-LB", "łódzkie"],
-    ["PL-MA", "małopolskie"],
-    ["PL-MZ", "mazowieckie"],
-    ["PL-OP", "opolskie"],
-    ["PL-PK", "podkarpackie"],
-    ["PL-PD", "podlaskie"],
-    ["PL-PM", "pomorskie"],
-    ["PL-SL", "śląskie"],
-    ["PL-SK", "świętokrzyskie"],
-    ["PL-WN", "warmińsko-mazurskie"],
-    ["PL-WP", "wielkopolskie"],
-    ["PL-ZP", "zachodniopomorskie"]
+  ["PL-DS", {name: "dolnośląskie", unitID: "x"}],
+  ["PL-KP", {name: "kujawsko-pomorskie", unitID: "x"}],
+  ["PL-LD", {name: "lubelskie", unitID: "x"}],
+  ["PL-LU", {name: "lubuskie", unitID: "x"}],
+  ["PL-LB", {name: "łódzkie", unitID: "x"}],
+  ["PL-MA", {name: "małopolskie", unitID: "x"}],
+  ["PL-MZ", {name: "mazowieckie", unitID: "x"}],
+  ["PL-OP", {name: "opolskie", unitID: "x"}],
+  ["PL-PK", {name: "podkarpackie", unitID: "x"}],
+  ["PL-PD", {name: "podlaskie", unitID: "x"}],
+  ["PL-PM", {name: "pomorskie", unitID: "x"}],
+  ["PL-SL", {name: "śląskie", unitID: "x"}],
+  ["PL-SK", {name: "świętokrzyskie", unitID: "x"}],
+  ["PL-WN", {name: "warmińsko-mazurskie", unitID: "x"}],
+  ["PL-WP", {name: "wielkopolskie", unitID: "x"}],
+  ["PL-ZP", {name: "zachodniopomorskie", unitID: "x"}]
 ]);
+
+function setUnitID() {
+  axios.get('https://cors-anywhere.herokuapp.com/https://bdl.stat.gov.pl/api/v1/units?level=2&format=json&page-size=20')
+    .then(function (response) {
+      console.log("TAK");
+      data = response.data;
+      if (data.totalRecords === 16) {
+        console.log(data.results);
+        voivodeships.forEach(voivodeship => {
+          data.results.forEach(data => {
+            if (data.name.toLowerCase() === voivodeship.name) {
+              voivodeship.unitID = data.id;
+            }
+          })
+        })
+      } else {
+        console.log("No data :(");
+      }
+    })
+    .catch(function (error) {
+      console.log("NIE");
+      console.log(error);
+    })
+}
+
+setUnitID();
+
 
 const map = document.querySelectorAll("path");
 const voivodeship = document.querySelector(".voivodeship");
 map.forEach(land => {
-    land.addEventListener("click", (e) => {
-        map.forEach(land => {
-            land.classList.remove("landTagged");
-        });
-        e.target.classList.add("landTagged");
-        voivodeship.textContent = voivodeships.get(e.target.id);
-    })
+  land.addEventListener("click", (e) => {
+    map.forEach(land => {
+      land.classList.remove("landTagged");
+    });
+    e.target.classList.add("landTagged");
+    voivodeship.textContent = voivodeships.get(e.target.id).name;
+  })
 })
