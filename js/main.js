7fcdf3f23779dab538af6ed689a43cf03ca108ae
Daniel Slowik <kkasztann@gmail.com>
2019-03-29 15:50:40 +0100

    Implemented OOP

--- js/main.js
+++ js/main.js
@@ -1,57 +1,86 @@
-const voivodeships = new Map([
-  ["PL-DS", {name: "dolnośląskie", unitID: "x"}],
-  ["PL-KP", {name: "kujawsko-pomorskie", unitID: "x"}],
-  ["PL-LD", {name: "lubelskie", unitID: "x"}],
-  ["PL-LU", {name: "lubuskie", unitID: "x"}],
-  ["PL-LB", {name: "łódzkie", unitID: "x"}],
-  ["PL-MA", {name: "małopolskie", unitID: "x"}],
-  ["PL-MZ", {name: "mazowieckie", unitID: "x"}],
-  ["PL-OP", {name: "opolskie", unitID: "x"}],
-  ["PL-PK", {name: "podkarpackie", unitID: "x"}],
-  ["PL-PD", {name: "podlaskie", unitID: "x"}],
-  ["PL-PM", {name: "pomorskie", unitID: "x"}],
-  ["PL-SL", {name: "śląskie", unitID: "x"}],
-  ["PL-SK", {name: "świętokrzyskie", unitID: "x"}],
-  ["PL-WN", {name: "warmińsko-mazurskie", unitID: "x"}],
-  ["PL-WP", {name: "wielkopolskie", unitID: "x"}],
-  ["PL-ZP", {name: "zachodniopomorskie", unitID: "x"}]
-]);
+// ======================== MAIN CLASS =====================
+class Main {
+  constructor(products = [], voivodeships = [], activeVoivodeship = {}) {
+    this.products = products;
+    this.voivodeships = voivodeships;
+    this.activeVoivodeship = activeVoivodeship;
 
-function setUnitID() {
-  axios.get('https://cors-anywhere.herokuapp.com/https://bdl.stat.gov.pl/api/v1/units?level=2&format=json&page-size=20')
-    .then(function (response) {
-      console.log("TAK");
-      data = response.data;
-      if (data.totalRecords === 16) {
-        console.log(data.results);
-        voivodeships.forEach(voivodeship => {
-          data.results.forEach(data => {
+    this.map = document.querySelectorAll("path");
+  }
+
+
+  setVoivodeships() {
+    this.voivodeships = new Map([
+      ["PL-DS", new Voivodeship("dolnośląskie")],
+      ["PL-KP", new Voivodeship("kujawsko-pomorskie")],
+      ["PL-LD", new Voivodeship("lubelskie")],
+      ["PL-LU", new Voivodeship("lubuskie")],
+      ["PL-LB", new Voivodeship("łódzkie")],
+      ["PL-MA", new Voivodeship("małopolskie")],
+      ["PL-MZ", new Voivodeship("mazowieckie")],
+      ["PL-OP", new Voivodeship("opolskie")],
+      ["PL-PK", new Voivodeship("podkarpackie")],
+      ["PL-PD", new Voivodeship("podlaskie")],
+      ["PL-PM", new Voivodeship("pomorskie")],
+      ["PL-SL", new Voivodeship("śląskie")],
+      ["PL-SK", new Voivodeship("świętokrzyskie")],
+      ["PL-WN", new Voivodeship("warmińsko-mazurskie")],
+      ["PL-WP", new Voivodeship("wielkopolskie")],
+      ["PL-ZP", new Voivodeship("zachodniopomorskie")]
+    ]);
+  };
+
+
+  setVoivodeshipsOnClick() {
+    this.map.forEach(voivodeship => {
+      voivodeship.addEventListener("click", (e) => {
+        this.map.forEach(voivodeship => {
+          voivodeship.classList.remove("landTagged");
+        });
+        e.target.classList.add("landTagged");
+        document.querySelector(".voivodeship").textContent = this.voivodeships.get(e.target.id).name;
+        this.activeVoivodeship = this.voivodeships.get(e.target.id).unitID;
+      })
+    })
+
+  };
+
+
+  setVoivodeshipsUnit() {
+    axios.get('https://cors-anywhere.herokuapp.com/https://bdl.stat.gov.pl/api/v1/units?level=2&format=json&page-size=20', {
+      headers: {
+        'X-ClientId': '785f193c-9495-41ae-9e16-08d6b36cba0f'
+      }
+    }).then((response) => {
+      if (response.data.totalRecords === 16) {
+        console.log("16 rekordów");
+        this.voivodeships.forEach(voivodeship => {
+          response.data.results.forEach(data => {
             if (data.name.toLowerCase() === voivodeship.name) {
               voivodeship.unitID = data.id;
             }
           })
         })
       } else {
-        console.log("No data :(");
+        console.log("No data");
       }
-    })
-    .catch(function (error) {
-      console.log("NIE");
+    }).catch(function (error) {
       console.log(error);
     })
+  }
+}
+// ======================== VOIVODESHIP CLASS =====================
+class Voivodeship {
+  constructor(name, unitID = "no Data") {
+    this.name = name;
+    this.unitID = unitID;
+  }
 }
 
-setUnitID();
 
 
-const map = document.querySelectorAll("path");
-const voivodeship = document.querySelector(".voivodeship");
-map.forEach(land => {
-  land.addEventListener("click", (e) => {
-    map.forEach(land => {
-      land.classList.remove("landTagged");
-    });
-    e.target.classList.add("landTagged");
-    voivodeship.textContent = voivodeships.get(e.target.id).name;
-  })
-})
+// ======================== START HERE =====================
+main = new Main();
+main.setVoivodeships();
+main.setVoivodeshipsUnit();
+main.setVoivodeshipsOnClick();
